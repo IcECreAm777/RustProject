213374a4432cdb3c92b20d85c3ed63d2fa230ef4
icecream777 <hgk.1998@googlemail.com>
2020-06-01 14:03:07 +0200

    init gg ez

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 mod default_structures;
 
-use crate::default_structures::pokemon;
+use crate::default_structures::{pokemon, attacks, team_picking, battle};
+
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
+use ggez::graphics;
+use ggez::conf::WindowSetup;
+use std::path::Path;
 
 fn main() {
-    let b = pokemon::bulbasur();
-    println!("{}", b.name);
-    println!("{}", b.m1.name);
-    println!();
+    // Make a Context and an EventLoop.
+    let (mut ctx, mut event_loop) =
+    ContextBuilder::new("Pokemon Rust edition", "Niklas Rosseck | Killian Woick | Henning Gütschow")
+        .window_setup(WindowSetup::default().title("Pokemon Stadium - Rust Edition").icon(""))
+        .build()
+        .unwrap();
+
+    //init game
+    let mut my_game = PokemonGame::new(&mut ctx);
+
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e)
+    }
+}
+
+struct PokemonGame {
+    //TODO implement state - data for the game
+}
+
+impl PokemonGame {
+    pub fn new(_ctx: &mut Context) -> PokemonGame {
+        // Load/create resources here: images, fonts, sounds, etc.
+        PokemonGame { }
+    }
+}
+
+impl EventHandler for PokemonGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+
+        // Draw code here...
+
+        graphics::present(ctx)
+    }
 }

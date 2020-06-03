06ee00bc0823c338269627a464e10d0a8b404ac9
Nerv <kilian.woick@gmail.com>
2020-06-03 22:00:06 +0200

    Basic attack inititation & dmg calc, dead check

--- src/default_structures/battle.rs
+++ src/default_structures/battle.rs
@@ -54,8 +54,8 @@ impl Battle {
 
     pub fn battle_phase(&mut self) {
         //TODO implement battle logic
-
-        //swap
+    
+        //flags/other stuff cuz Rust is weird
         let mut swap1: bool = false;
         let mut swap2: bool = false;
         let mut to_switch: Battlemon = Battlemon {
@@ -64,25 +64,77 @@ impl Battle {
                                             evasion: 0,
                                             accuracy: 0,
                                         };
+        let mut atk1: bool = false;
+        let mut atk2: bool = false;
+        let mut to_atk1: attacks::Attack = attacks::dummy();
+        let mut to_atk2: attacks::Attack = attacks::dummy();
+        let mut dead = 0;
+
+        //match and if swap do so immediately
         match self.a1 {
             Action::Swap (pok) => {swap1 = true; to_switch = pok;},
+            Action::Attack (atk) => {atk1 = true; to_atk1 = atk;}
             _ => {},
         };
         if swap1 == true {
             Battle::swap_pokemon(&mut self.p1, &mut to_switch);
+            //swap1 = false;
         }
 
         match self.a2 {
             Action::Swap (pok) => {swap2 = true; to_switch = pok;},
+            Action::Attack (atk) => {atk2 = true; to_atk2 = atk;}
             _ => {},
         };
         if swap2 == true {
             Battle::swap_pokemon(&mut self.p2, &mut to_switch);
+            //swap2 = false;
         }
 
         //attack
 
+        if atk1 == true {
+            Battle::attacks(&to_atk1, &mut self.p1, &mut self.p2);
+            //atk1 = false;
+            for i in self.own_team.iter() {         // check if either team dead
+                if i.current_health == 0 {
+                    dead += 1;
+                }
+            }
+            if dead == 6 {return}
+            else {dead = 0;}
+            for i in self.enemy_team.iter() {
+                    if i.current_health == 0 {
+                        dead += 1;
+                    }
+            }
+            if dead == 6 {return}
+            else {dead = 0;}
+        }
+
+        if atk2 == true {
+            Battle::attacks(&to_atk2, &mut self.p2, &mut self.p1);
+            //atk2 = false;
+            for i in self.own_team.iter() {         // again check if either team dead
+                if i.current_health == 0 {
+                    dead += 1;
+                }
+            }
+            if dead == 6 {return}
+            else {dead = 0;}
+            for i in self.enemy_team.iter() {
+                if i.current_health == 0 {
+                    dead += 1;
+                }
+            }
+            if dead == 6 {return}
+        }
+
+        // TODO: implement check to return if one team is completely dead after each atk
+
         //effects (burn, sleep, etc.)
+
+        // TODO: again check if either team dead after effects
 
         self.a1 = Action::Picking;
         self.a2 = Action::Picking;
@@ -92,14 +144,12 @@ impl Battle {
         *current = *pok;
     } //TODO implement
 
-//    pub fn swap_enemy_pokemon(&mut self, pok: Battlemon) {
-//        self.p2 = pok;
-//    } //TODO implement
-
-    pub fn attacks(attack: attacks::Attack, target: Battlemon) {
-        //let multiplier; -> look in hashmap of type for pokemon types and set it accordingly
-
-        //damage calculation
+    pub fn attacks(attack: &attacks::Attack, user: &Battlemon, target: &mut Battlemon) {
+        // for now: just basic damage calculation
+        // TODO effect checks like constant damage/status
+        // TODO differentiate physical/special, include status etc.
+        target.current_health -= 42*attack.strength*(user.pokemon.atk/(50*target.pokemon.def))+2;  //TODO F1, crit, stab, effectiveness
+        // TODO check for effects after dmg calc
     }
 
     pub fn effect() {

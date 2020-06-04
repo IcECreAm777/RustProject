f7a594d62cfdbfa0caee6709e8f16f32cfb6d12d
Nerv <kilian.woick@gmail.com>
2020-06-04 22:48:40 +0200

    Added effectiveness maps

--- src/default_structures.rs
+++ src/default_structures.rs
@@ -4,6 +4,7 @@ pub mod battle;
 pub mod team_picking;
 
 use std::fmt::{Display, Result, Formatter};
+use std::collections::HashMap;
 
 #[derive(Copy, Clone, PartialEq, Eq, std::hash::Hash)]
 pub enum Type {
@@ -24,6 +25,38 @@ pub enum Type {
     Water,
     None
 }
+// initialize HashMaps for type effectivenesses
+static BUG: Type = Type::Bug;
+static DRAGON: Type = Type::Dragon;
+static ELECTRIC: Type = Type::Electric;
+static FIGHTING: Type = Type::Fighting;
+static FIRE: Type = Type::Fire;
+static FLYING: Type = Type::Flying;
+static GHOST: Type = Type::Ghost;
+static GRASS: Type = Type::Grass;
+static GROUND: Type = Type::Ground;
+static ICE: Type = Type::Ice;
+static NORMAL: Type = Type::Normal;
+static POISON: Type = Type::Poison;
+static PSYCHIC: Type = Type::Psychic;
+static ROCK: Type = Type::Rock;
+static WATER: Type = Type::Water;
+
+pub fn normap() -> HashMap<Type,f32> {let map = [(ROCK,0.5),(GHOST,0.0)].iter().cloned().collect(); map}
+pub fn fimap() -> HashMap<Type,f32> {let map = [(FIRE,0.5),(WATER,0.5),(GRASS,2.0),(ICE,2.0),(BUG,2.0),(ROCK,0.5),(DRAGON,0.5)].iter().cloned().collect(); map}
+pub fn wamap() -> HashMap<Type,f32> {let map = [(FIRE,2.0),(WATER,0.5),(GRASS,0.5),(GROUND,2.0),(ROCK,2.0),(DRAGON,0.5)].iter().cloned().collect(); map}
+pub fn elmap() -> HashMap<Type,f32> {let map = [(WATER,2.0),(ELECTRIC,0.5),(GRASS,0.5),(GROUND,0.0),(FLYING,2.0),(DRAGON,0.5)].iter().cloned().collect(); map}
+pub fn gramap() -> HashMap<Type,f32> {[(FIRE,0.5),(WATER,2.0),(GRASS,0.5),(POISON,0.5),(GROUND,2.0),(FLYING,0.5),(BUG,0.5),(ROCK,2.0),(DRAGON,0.5)].iter().cloned().collect(); map}
+pub fn icemap() -> HashMap<Type,f32> {[(WATER,0.5),(GRASS,2.0),(ICE,0.5),(GROUND,2.0),(FLYING,2.0),(DRAGON,2.0)].iter().cloned().collect(); map}
+pub fn figmap() -> HashMap<Type,f32> {[(NORMAL,2.0),(ICE,2.0),(POISON,0.5),(FLYING,0.5),(PSYCHIC,0.5),(BUG,0.5),(ROCK,2.0),(GHOST,0.0)].iter().cloned().collect(); map}
+pub fn poimap() -> HashMap<Type,f32> {[(GRASS,2.0),(POISON,0.5),(GROUND,0.5),(BUG,0.5),(ROCK,0.5),(GHOST,0.5)].iter().cloned().collect(); map}
+pub fn gromap() -> HashMap<Type,f32> {[(FIRE,2.0),(ELECTRIC,2.0),(GRASS,0.5),(POISON,2.0),(FLYING,0.0),(BUG,0.5)].iter().cloned().collect(); map}
+pub fn flymap() -> HashMap<Type,f32> {[(ELECTRIC,0.5),(GRASS,2.0),(FIGHTING,2.0),(BUG,2.0),(ROCK,0.5)].iter().cloned().collect(); map}
+pub fn psymap() -> HashMap<Type,f32> {[(FIGHTING,2.0),(POISON,2.0),(PSYCHIC,0.5),(GHOST,2.0)].iter().cloned().collect(); map}
+pub fn bugmap() -> HashMap<Type,f32> {let map = [(FIRE,0.5),(GRASS,2.0),(FIGHTING,0.5),(POISON,2.0),(FLYING,0.5),(PSYCHIC,2.0),(GHOST,0.5)].iter().cloned().collect(); map}
+pub fn rockmap() -> HashMap<Type,f32> {let map = [(FIRE,2.0),(ICE,2.0),(FIGHTING,0.5),(GROUND,0.5),(FLYING,2.0),(BUG,2.0)].iter().cloned().collect(); map}
+pub fn ghomap() -> HashMap<Type,f32> {let map = [(NORMAL,0.0),(PSYCHIC,2.0),(GHOST,2.0)].iter().cloned().collect(); map}
+pub fn dramap() -> HashMap<Type,f32> {let map = [(DRAGON,2.0)].iter().cloned().collect(); map}
 
 impl Display for Type {
     fn fmt(&self, f: &mut Formatter) -> Result {
